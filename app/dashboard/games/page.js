024726d8728c1cdc5082ac19efafee5ed77ad371
DestinyDriver <destinydriverx@gmail.com>
2026-02-02 22:47:29 +0530

    Add: Made basic configurations for the games section

---
+++ app/dashboard/games/page.js
@@ -0,0 +1,12 @@
+import React from "react";
+import EmotionGame from "@/app/components/games/EmotionGame";
+
+const page = () => {
+  return (
+    <div>
+      <EmotionGame></EmotionGame>
+    </div>
+  );
+};
+
+export default page;
